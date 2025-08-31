88fece5920db4dc25b27ac899a7c52a7e7495aad
faisal <faisalabdalnasser275@gmail.com>
2025-08-31 12:42:17 +0300

    create project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SmartHomeApp());
+}
+
+class SmartHomeApp extends StatelessWidget {
+  const SmartHomeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
