876b07c3226e14d41fe0a03df725bcdada4c5e13
faisal <faisalabdalnasser275@gmail.com>
2025-09-02 17:27:14 +0300

    connect with Mqtt server

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,26 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
 
 void main() {
+  MqttServerClient client = MqttServerClient.withPort('broker.hivemq.com', '1', 1883);
+
+  MqttClientPayloadBuilder builder = MqttClientPayloadBuilder();
+  client.connect().then((value) => {
+    if(value!.state == client.connectionStatus!.state) {
+
+      client.subscribe("test/flutter", MqttQos.atMostOnce),
+
+      client.publishMessage("test/flutter", MqttQos.atMostOnce, builder.addString("Hello from Flutter").payload!),
+
+      log('Connected to MQTT broker'),
+      
+    } else {
+      log('Failed to connect, status is ${value.state}')
+    }
+  });
   runApp(const SmartHomeApp());
 }
 
